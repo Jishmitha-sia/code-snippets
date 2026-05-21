cb8001c82eca595b78b02008cbba4681e50cc02c
Jishmitha-sia <jishmitha0305@gmail.com>
2026-05-21 21:52:43 +0530

    Added missing number program

---
+++ MissingNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 0; i < n - 1; i++)
+            sum += sc.nextInt();
+
+        System.out.println(n * (n + 1) / 2 - sum);
+
+        sc.close();
+    }
+}

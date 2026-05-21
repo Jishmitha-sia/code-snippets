548311f220adbbb3b7cd9f7342ca5766629d3712
Jishmitha-sia <jishmitha0305@gmail.com>
2026-05-21 21:50:51 +0530

    Added pascals triangle program

---
+++ PascalsTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+
+        for(int i = 0; i < rows; i++) {
+
+            int num = 1;
+
+            for(int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}

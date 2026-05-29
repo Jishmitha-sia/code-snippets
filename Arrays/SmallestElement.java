638de197f7f48573e7e1dc730c61c018a2631b2a
Jishmitha-sia <jishmitha0305@gmail.com>
2026-05-29 19:49:18 +0530

    Added smallest element program

---
+++ Arrays/SmallestElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), arr[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int min = arr[0];
+
+        for(int num : arr)
+            if(num < min)
+                min = num;
+
+        System.out.println(min);
+    }
+}
